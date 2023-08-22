aeef37842e9ac299f3085c09975bc335c2de1db2
Arn Van Hemelen <arn.vanhemelen@student.uantwerpen.be>
2023-08-22 08:48:46 +0200

    final adaptations

--- mandelbrot/different_cases/50_100.py
+++ mandelbrot/different_cases/50_100.py
@@ -1,26 +1,26 @@
-import numpy
+import numpy as np
 from PIL import Image, ImageDraw
 import time
 from mpi4py import MPI
 
-def mandelbrot(c, max_iter):
+def mandelbrot(c):
     # Check whether complex number belongs to Mandelbrot set
     # Returns the number of iterations
     z = c
     for n in range(max_iter):
-        if z.real * z.real + z.imag * z.imag > 4.0::
+        if z.real * z.real + z.imag * z.imag > 4.0:
             return n
         z = z*z + c
     return max_iter
 
-def make_color(color, ondergrens, bovengrens, H, W, max_iter):
+def make_color(color, ondergrens, bovengrens, H, W):
     for x in range(ondergrens, bovengrens):
         for y in range(0, H):
         # PIXEL COORDINATE TO COMPLEX NUMBER
             c = complex(-2 + (x / W) * (3),
                 0 + (y / H) * (1.333))
             # CALCULATE NUMBER OF ITERATIONS
-            m = mandelbrot(c, max_iter)
+            m = mandelbrot(c)
             # print("[", x, y, "]:", m)
             hue=int(255 * m / max_iter)
             saturation=(int(255))
@@ -28,7 +28,7 @@ def make_color(color, ondergrens, bovengrens, H, W, max_iter):
             color[x,y] = [hue, saturation, value]
     return color
 
-def make_image(image, color, draw, pixel, max_iter):
+def make_image(image, color, draw):
     for key in color:
         draw.point([key[0], key[1]], (color[key][0], color[key][1], color[key][2]))
     image.convert('RGB').save('MB_' + str(pixel) + str(max_iter) + '.png', 'PNG')
@@ -48,17 +48,17 @@ def main():
     # start with node 0 and work up to node 63
     # then send back data to node 0
     if rank == 0:
-        data = make_color(color, ondergrens, bovengrens, H, W, max_iter)
+        data = make_color(color, ondergrens, bovengrens, H, W)
         comm.send(data, dest = (rank+1)%size)
     if rank > 0:
         if rank == 63: 
             bovengrens = W
         data = comm.recv(source=(rank-1)%size)
-        data_rank = make_color(data, ondergrens, bovengrens, H, W, max_iter)
+        data_rank = make_color(data, ondergrens, bovengrens, H, W)
         comm.send(data_rank, dest=(rank+1)%size)
     if rank == 0:
         data_rank = comm.recv(source = size-1)
-        make_image(image, data_rank, draw, pixel, max_iter)
+        make_image(image, data_rank, draw)
 
 if __name__ == '__main__':
 
